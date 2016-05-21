1de6961304fb127ad529f90cfba45a5be7c4b982
Sille Van Landschoot <info@sillevl.be>
2016-05-21 15:19:08 +0000

    initial working version

--- lib/mazebots.rb
+++ lib/mazebots.rb
@@ -1,5 +1,49 @@
 require "mazebots/version"
 
+require "touchberrypi"
+require "m3pi"
+
 module Mazebots
-  # Your code goes here...
+
+	class Mazebots
+
+		include Touchberrypi
+		include M3pi
+
+		def initialize
+			keyboard = Keyboard.new
+
+			serial = SerialPort.new '/dev/ttyUSB0', 9600
+			#serial.open
+
+			robot = Robot.new [0x00,0x13,0xA2,0x00,0x40,0xB4,0x10,0x5D], serial
+
+			direction = "stop"
+
+			keyboard.on_key_down do |key|
+			direction = key
+			end
+
+			keyboard.on_key_up do |key|
+				direction = "stop"
+			end
+
+			loop do
+				#puts direction
+				case direction
+				when "UP" #forward
+					robot.forward
+				when 'LEFT' #left
+					robot.left
+				when 'DOWN' #back
+				        robot.reverse
+				when 'RIGHT' #right
+					robot.right
+				end
+				sleep 0.05			
+			end
+		end
+	end
+
 end
+
---
+++ test/test.rb
@@ -0,0 +1,7 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require "mazebots"
+
+include Mazebots
+
+app = Mazebots::Mazebots.new
+
